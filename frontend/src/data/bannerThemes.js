95df9039d85218a0ca1ed5b7e6a55fb313123fd0
jkof86 <cisjay@gmail.com>
2026-01-12 08:12:29 -0500

    Added cusom userType-themed Logos to UserHeader

--- frontend/src/data/bannerThemes.js
+++ frontend/src/data/bannerThemes.js
@@ -1,31 +1,31 @@
 // bannerThemes.js
-import googleLogo from "../images/bg/ksLogo4.png"; 
-import appleLogo from "../images/bg/ksLogo1.png"; 
-import guestLogo from "../images/bg/ksLogo2.png";
+import googleLogo from "../images/bg/ksLogo2T.png"; 
+import appleLogo from "../images/bg/ksLogo1T.png"; 
+import guestLogo from "../images/bg/ksLogo4T.png";
 
 export const BannerThemes = {
     google: {
         gradient: "linear-gradient(to right, #e82020a5, #fbe41abc, #34a853bf, #1b1bf4b5)",
         background: "#5c5c5c",
-        image: "googleLogo",
+        image: googleLogo,
         textColor: "#204b29"
     },
     apple: {
         gradient: "linear-gradient(45deg, #1976d2, #93b4d5)",
         background: "#ffffff",
-        image: "appleLogo",
+        image: appleLogo,
         textColor: "#204b29"
     },
     guest: {
-        gradient: "linear-gradient(to right, #1976d2, #1976d2)",
+        gradient: "linear-gradient(to right, #eceef1, #1976d2)",
         background: "#ffffff",
-        image: "guestLogo",
+        image: guestLogo,
         textColor: "#000000"
     },
     default: {
         gradient: "linear-gradient(to right, #1e3c72, #3b78e2)",
         background: "#ffffff",
-        image: "guestLogo",
+        image: guestLogo,
         textColor: "#000000"
     }
 };
