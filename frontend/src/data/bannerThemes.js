1f47baf65acb871a8ef75492d44844165d9d00cd
jkof86 <cisjay@gmail.com>
2026-01-16 22:55:47 -0500

    adjust color gradient

--- frontend/src/data/bannerThemes.js
+++ frontend/src/data/bannerThemes.js
@@ -5,7 +5,7 @@ import guestLogo from "../images/bg/ksLogo4T.png";
 
 export const BannerThemes = {
     google: {
-        gradient: "linear-gradient(to right,  #34a853bf, #fdd7d7)",
+        gradient: "linear-gradient(to right,  #34a853bf, #ffffff)",
         background: "#5c5c5c",
         image: googleLogo,
         textColor: "#204b29"
